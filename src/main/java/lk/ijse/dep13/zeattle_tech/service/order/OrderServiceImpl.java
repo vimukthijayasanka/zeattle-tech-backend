5b1ede77efe7c7ba99dc9ac9bcf9b2bf0fbeb8ff
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-16 19:44:22 +0530

    feat: implement order item creation and stock update logic during order placement

--- src/main/java/lk/ijse/dep13/zeattle_tech/service/order/OrderServiceImpl.java
+++ src/main/java/lk/ijse/dep13/zeattle_tech/service/order/OrderServiceImpl.java
@@ -1,17 +1,69 @@
 package lk.ijse.dep13.zeattle_tech.service.order;
 
+import lk.ijse.dep13.zeattle_tech.entity.Cart;
 import lk.ijse.dep13.zeattle_tech.entity.Order;
+import lk.ijse.dep13.zeattle_tech.entity.OrderItem;
+import lk.ijse.dep13.zeattle_tech.entity.Product;
+import lk.ijse.dep13.zeattle_tech.enums.OrderStatus;
+import lk.ijse.dep13.zeattle_tech.exception.ResourceNotFoundException;
+import lk.ijse.dep13.zeattle_tech.repository.OrderRepository;
+import lk.ijse.dep13.zeattle_tech.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.nio.file.ReadOnlyFileSystemException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
+
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+
     @Override
     public Order placeOrder(Long userId) {
         return null;
     }
 
+    private Order createOrder(Cart cart) {
+        Order order = new Order();
+        // TODO set the user...
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDate.now());
+        order.setOrderTime(LocalTime.now());
+        return order;
+    }
+
+    private List<OrderItem> createOrderItems(Order order, Cart cart) {
+        return cart.getCartItems().stream().map(
+                cartItem -> {
+                    Product product = cartItem.getProduct();
+                    // Reduce stock after making the order
+                    product.setStock(product.getStock() - cartItem.getQuantity());
+                    productRepository.save(product);
+                    return new OrderItem(
+                            order,
+                            product,
+                            cartItem.getQuantity(),
+                            cartItem.getUnitPrice()
+                    );
+                }).toList();
+    }
+
+    private BigDecimal calculateTotalAmount(List<OrderItem> orderItemsList) {
+        return orderItemsList.stream().map(item ->
+                         item.getPrice()
+                        .multiply(new BigDecimal(item.getQuantity())))
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
     @Override
     public Order getOrder(Long orderId) {
-        return null;
+        return orderRepository.findById(orderId)
+                .orElseThrow(()->new ResourceNotFoundException("Order not found"));
     }
 }
