bb6684fb1c25cf9cb99f2cf4d364b1553839ef68
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-20 12:40:29 +0530

    Add configurations for Authentications and security

--- src/main/java/lk/ijse/dep13/zeattle_tech/config/WebAppConfig.java
+++ src/main/java/lk/ijse/dep13/zeattle_tech/config/WebAppConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep13.zeattle_tech.config;
 
+import lk.ijse.dep13.zeattle_tech.security.jwt.JwtAuthEntryPoint;
+import lk.ijse.dep13.zeattle_tech.service.user.ShopUserDetailService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
---
+++ src/main/java/lk/ijse/dep13/zeattle_tech/security/config/ShopConfig.java
@@ -0,0 +1,65 @@
+package lk.ijse.dep13.zeattle_tech.security.config;
+
+import lk.ijse.dep13.zeattle_tech.security.jwt.AuthTokenFilter;
+import lk.ijse.dep13.zeattle_tech.security.jwt.JwtAuthEntryPoint;
+import lk.ijse.dep13.zeattle_tech.service.user.ShopUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ShopConfig {
+    private static final List<String> SECURED_URLS = List.of("");
+    private final ShopUserDetailService shopUserDetailService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(shopUserDetailService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(SECURED_URLS.toArray(String[]::new)).authenticated()
+                        .anyRequest().permitAll());
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
